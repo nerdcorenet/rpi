ab08fc642beaa340b0f0436709b697c4a156f2ba
mike <mike@nerdcore.net>
2020-11-18 22:51:31 -0500

    Add RGB

---
+++ rgb/rgb_pwm.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+import time
+import math
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+
+PIN_R=14
+PIN_G=15
+PIN_B=18
+PWM_FREQ=2000
+
+GPIO.setup(PIN_R, GPIO.OUT)
+GPIO.setup(PIN_G, GPIO.OUT)
+GPIO.setup(PIN_B, GPIO.OUT)
+
+r=GPIO.PWM(PIN_R, PWM_FREQ)
+g=GPIO.PWM(PIN_G, PWM_FREQ)
+b=GPIO.PWM(PIN_B, PWM_FREQ)
+
+r.start(50)
+b.start(50)
+g.start(50)
+
+count = 0
+while 1:
+    s = math.sin(count)
+    s2 = math.sin(count*2)
+    s3 = math.sin(count*8)
+
+    duty = (s + 1) * 50
+
+    r.ChangeDutyCycle(duty)
+    g.ChangeDutyCycle((s2 + 1)*50)
+    b.ChangeDutyCycle((s3 + 1)*50)
+    #r.ChangeFrequency(duty*10)
+    time.sleep(0.25)
+    count = count + 1
